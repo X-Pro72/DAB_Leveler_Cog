0bc72f55c9dcf82cd40aab2f14e6b088a3179395
X-Pro72 <99927666+X-Pro72@users.noreply.github.com>
2023-05-07 17:27:48 -0400

    Update __init__.py

--- Leveler/__init__.py
+++ Leveler/__init__.py
@@ -2,4 +2,6 @@ from .leveler import Leveler
 
 
 async def setup(bot):
-    await bot.add_cog(Leveler(bot))
+    n = Leveler(bot)
+    bot.add_listener(n.listener, "on_message")
+    await bot.add_cog(n)
